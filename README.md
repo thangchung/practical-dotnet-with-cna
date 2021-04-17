8e27ae3a81cf651003c4dcd1737ca90a55e40e5b
thangchung <thangchung@ymail.com>
2021-04-17 17:32:51 +0700

    update with new pack (env)

--- README.md
+++ README.md
@@ -1,11 +1,31 @@
 # practical-dotnet-with-cna
 
+## Build with CNCF `buildpacks`
+
+### Preparation
+
 ```bash
 $ choco install pack --version=0.18.0
 ```
 
 PaketoBuildpacks: https://github.com/paketo-buildpacks/samples/tree/main/dotnet-core
 
+### Build services
+
+#### Service 1
+
 ```bash
-$ pack build webapi --buildpack gcr.io/paketo-buildpacks/dotnet-core
+$ pack build vietnamdevsgroup/webapi --env BP_DOTNET_PROJECT_PATH=./service1/src/WebApi --buildpack gcr.io/paketo-buildpacks/dotnet-core
+```
+
+#### Service 2
+
+```bash
+$ pack build vietnamdevsgroup/webapi2 --env BP_DOTNET_PROJECT_PATH=./service2/src/WebApi2 --buildpack gcr.io/paketo-buildpacks/dotnet-core
+```
+
+#### Run all services
+
+```bash
+$ docker-compose up
 ```
--- service2/src/WebApi2/Startup.cs
+++ service2/src/WebApi2/Startup.cs
@@ -13,44 +13,44 @@ using System.Threading.Tasks;
 
 namespace WebApi
 {
-		public class Startup
-		{
-				public Startup(IConfiguration configuration)
-				{
-						Configuration = configuration;
-				}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-				public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-				// This method gets called by the runtime. Use this method to add services to the container.
-				public void ConfigureServices(IServiceCollection services)
-				{
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
 
-						services.AddControllers();
-						services.AddSwaggerGen(c =>
-						{
-								c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
-						});
-				}
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
+        }
 
-				// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-				public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-				{
-						if (env.IsDevelopment())
-						{
-								app.UseDeveloperExceptionPage();
-								app.UseSwagger();
-								app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
-						}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+            }
 
-						app.UseRouting();
+            app.UseRouting();
 
-						app.UseAuthorization();
+            app.UseAuthorization();
 
-						app.UseEndpoints(endpoints =>
-						{
-								endpoints.MapControllers();
-						});
-				}
-		}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
